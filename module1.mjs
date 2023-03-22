b5b4ba6b479c818ed0f9b2293be6685fa9a503e6
mayanksaini9737 <113924903+mayanksaini9737@users.noreply.github.com>
2023-03-22 15:34:51 +0530

    first upload

---
+++ module1.mjs
@@ -0,0 +1,11 @@
+const a = "Mayank";
+const b = "Deep";
+const c = "Azeem";
+const d = "Yogi";
+const e = "Harsh";
+
+export default a;
+export {b};
+export {c};
+export {d};
+export {e};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import mynk, {b, c, d, e} from './module1.mjs'
+console.log(mynk)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
